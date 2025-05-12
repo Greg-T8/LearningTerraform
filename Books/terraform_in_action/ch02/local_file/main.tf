af9b72b1a4aa7ab780c09e0af1f6b9b3bd354859
Greg Tate <gregt8@outlook.com>
2025-05-12 04:44:21 -0500

    Ch02: Update heredoc syntax for local_file resource content formatting

--- Books/terraform_in_action/ch02/local_file/main.tf
+++ Books/terraform_in_action/ch02/local_file/main.tf
@@ -10,11 +10,11 @@ terraform {
 
 resource "local_file" "literature" {
     filename = "art_of_war.txt"
-<<-EOT      # heredoc syntax; leading whitespace is ignored
-    Sun Tzu said: The art of war is of vital importance to the State.
+    content  = <<-EOT
+        Sun Tzu said: The art of war is of vital importance to the State.
 
-    It is a matter of life and death, a road either to safety or to
-    ruin. Hence it is a subject of inquiry which can on no account be
-    neglected.
+        It is a matter of life and death, a road either to safety or to
+        ruin. Hence it is a subject of inquiry which can on no account be
+        neglected.
     EOT
 }
