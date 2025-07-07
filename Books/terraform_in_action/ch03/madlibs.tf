b8e52060c0be97cdd46e9673d108a0e83dd7d1d6
Greg Tate <gregt8@outlook.com>
2025-07-07 04:58:11 -0500

    Ch03: Notes - Add local and archive providers, update mad libs resource to use templates, and create zip output of mad libs

--- Books/terraform_in_action/ch03/madlibs.tf
+++ Books/terraform_in_action/ch03/madlibs.tf
@@ -5,6 +5,14 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -57,13 +65,26 @@ resource "random_shuffle" "random_numbers" {
   input = local.uppercase_words["numbers"]
 }
 
-output "mad_libs" {
-  value = templatefile("${path.module}/templates/alice.txt",
+locals {
+  templates = tolist(fileset(path.module, "templates/*.txt"))
+}
+
+resource "local_file" "mad_libs" {
+  count    = var.num_files
+  filename = "madlibs/madlibs-${count.index}.txt"
+  content = templatefile(element(local.templates, count.index),
     {
-      nouns      = random_shuffle.random_nouns.result
-      adjectives = random_shuffle.random_adjectives.result
-      verbs      = random_shuffle.random_verbs.result
-      adverbs    = random_shuffle.random_adverbs.result
-      numbers    = random_shuffle.random_numbers.result
+      nouns      = random_shuffle.random_nouns[count.index].result
+      adjectives = random_shuffle.random_adjectives[count.index].result
+      verbs      = random_shuffle.random_verbs[count.index].result
+      adverbs    = random_shuffle.random_adverbs[count.index].result
+      numbers    = random_shuffle.random_numbers[count.index].result
   })
+}
+
+data "archive_file" "mad_libs" {
+  depends_on  = [local_file.mad_libs]
+  type        = "zip"
+  source_dir  = "${path.module}/madlibs"
+  output_path = "${path.cwd}/madlibs.zip"
 }
